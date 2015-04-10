03bfb6aca4a92a0b4cf1fed72601d2e311944cdb
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2015-04-10 15:32:58 -0300

    add carousel heade

---
+++ public/js/todoxchile.js
@@ -0,0 +1,78 @@
+  (function ($) {
+    "use strict";
+    var fn = {
+
+        // Launch Functions
+        Launch: function () {
+            fn.MainSlider();
+            fn.MainSliderAlign();
+            fn.Carousel();
+            fn.Slider();
+        },
+
+        // Align Slider Horizontally
+        MainSliderAlign: function () {
+            var imageWidth, widthFix, container = $('.header-bg');
+            function centerImage() {
+                imageWidth = container.find('img').width();
+                widthFix = imageWidth / 2;
+                container.find('img').css('margin-left', -widthFix);
+            }
+            $(window).on("load resize", centerImage);
+        },
+
+        // Main FlexSlider
+        MainSlider: function () {
+            $(window).load(function () {
+                $('.main-slider').flexslider({
+                    noCSS: true,
+                    touch: false,
+                    controlNav: false,
+                    directionNav: false,
+                    animation: "fade",
+                    start: function () {
+                        $('#preloader').addClass('ready');
+                    }
+                });
+            });
+        },
+
+        // Owl Carousel
+        Carousel: function () {
+            var owl = $("#carousel");
+            owl.owlCarousel({
+                itemsCustom : [
+                    [1200, 4],
+                    [970, 3],
+                    [768, 2],
+                    [360, 1]
+                ],
+                navigation : false
+            });
+
+            $(".next").click(function () {
+                owl.trigger('owl.next');
+            });
+
+            $(".prev").click(function () {
+                owl.trigger('owl.prev');
+            });
+        },
+
+        // FlexSlider
+        Slider: function () {
+            $('.flexslider').flexslider({
+                noCSS: true,
+                touch: false,
+                directionNav: false,
+                animation: "fade"
+            });
+        }
+
+    };
+
+    $(document).ready(function () {
+        fn.Launch();
+    });
+
+})(jQuery);
